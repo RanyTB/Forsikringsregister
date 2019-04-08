33e85564e065f28e5e28424e1f4cf20b11cee3cf
Eirik Bøyum <eirik_smile@hotmail.com>
2019-04-08 14:01:35 +0200

    Lagt til TODO csv

--- src/main/java/com/rtbeb/model/filemanagement/WriteToCSV.java
+++ src/main/java/com/rtbeb/model/filemanagement/WriteToCSV.java
@@ -1,6 +1,8 @@
 package com.rtbeb.model.filemanagement;
 
 public class WriteToCSV implements FileManagement{
+
+    //TODO implementer denne metoden
     @Override
     public void writeToFile(String path) {
 
