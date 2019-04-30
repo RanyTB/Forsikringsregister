b9d5dbcb496b4ce5e670c87693573b412fbe3f08
Rany Tarek Bouorm <rany-94@hotmail.com>
2019-04-30 18:00:05 +0200

    Legg til styling for prosjektet.

---
+++ src/main/java/com/rtbeb/controller/helper/FXMLStyler.java
@@ -0,0 +1,11 @@
+package com.rtbeb.controller.helper;
+
+import javafx.scene.Scene;
+
+public class FXMLStyler {
+
+    public static void addDefaultStyleSheet(Scene scene){
+        scene.getStylesheets().add("/styles/Styles.css");
+    }
+
+}
